1c05b44e89845611dd9725545f20289f96224e21
H-Zoon <jun34724@gmail.com>
2025-06-27 08:55:35 +0900

    version3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.devidea.aicar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
