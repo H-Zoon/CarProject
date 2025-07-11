6bb0d9cd87c7fa5a31b03116204358ee65933877
jun34723 <jun34723@avatye.com>
2025-07-11 14:18:42 +0900

    [develop] versionCode = 7 versionName = "1.1"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.devidea.aicar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0"
+        versionCode = 7
+        versionName = "1.1"
     }
 
     buildTypes {
